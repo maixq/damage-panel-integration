7df46f5ca53e9259e4820ca4dde4be92deaa33b7
maixq <mai.xueqiao@carro.co>
2022-09-12 10:28:32 +0800

    return damage percentage of panel

---
+++ detectors.py
@@ -0,0 +1,182 @@
+from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer
+from detectron2.config import get_cfg
+from detectron2.projects.point_rend import add_pointrend_config
+import detectron2.data.transforms as T
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import ColorMode
+import time
+import logging
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Detector Classs
+class PanelDetector():
+    def __init__(self, panel_config, panel_checkpoint, panel_threshold=0.9, model_device='cpu'):
+        self.panel_model = None
+        self.panel_classes = None
+        self.panel_class_metadata = None
+        self.panel_config = panel_config
+        self.panel_checkpoint = panel_checkpoint
+        self.panel_threshold = panel_threshold
+        self.model_device = model_device
+        self.initialise_panel()
+
+    def initialise_panel(self):
+
+        cfg = get_cfg()
+        add_pointrend_config(cfg)
+        cfg.merge_from_file(self.panel_config)
+        cfg.MODEL.WEIGHTS = self.panel_checkpoint
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.panel_threshold
+        cfg.MODEL.DEVICE = self.model_device
+        self.panel_model = DefaultPredictor(cfg)
+        logger.info('Panel Model Loaded')
+    
+    def predict(self, img):
+        '''
+        Function that takes in a Numpy Array and country code and returns an Masked Image or None
+        '''
+        t1 = time.time()
+        predictions = self.panel_model(img)
+        t2 = time.time()
+        logger.info('Panel Detection Model Inference took: {}'.format(str(t2 - t1)))
+
+        instances = predictions["instances"].to("cpu")
+
+        v = Visualizer(
+        img_rgb=img,
+        instance_mode=ColorMode.SEGMENTATION
+        )
+        
+        out = v.draw_instance_predictions(
+        instances
+        )
+            
+        result = {
+            'image' : out,
+        }
+
+        return result, instances
+
+# Car Detection class
+class CarDetector():
+    def __init__(self, config, checkpoint, threshold=0.9, model_device='cpu'):
+        self.model = None
+        self.config = config
+        self.checkpoint = checkpoint
+        self.threshold = threshold
+        self.model_device = model_device
+        self.initialise()
+
+    def initialise(self):
+        cfg = get_cfg()
+        add_pointrend_config(cfg)
+        cfg.merge_from_file(self.config)
+        cfg.MODEL.WEIGHTS = self.checkpoint
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
+        cfg.MODEL.DEVICE = self.model_device
+        self.model = DefaultPredictor(cfg)
+    
+    def predict(self, img):
+        start_time = time.time()
+        predictions = self.model(img)
+
+        # Get detection boxes
+        boxes = predictions['instances'].pred_boxes
+        masks = predictions['instances'].pred_masks
+
+        # If no boxes are detected, return original image and None for mask
+        if len(boxes) == 0:
+            return Image.fromarray(img), None
+        else:
+            # Crop Image
+            box = list(boxes)[0].detach().cpu().numpy()
+            cropped = self.crop_object(img, box)
+            
+            # Crop segmentation mask
+            mask = list(masks)[0].detach().cpu().numpy()
+            cropped_mask = self.crop_object(mask, box)
+            
+            return cropped, cropped_mask
+
+    def crop_object(self, image, box):
+        image = Image.fromarray(image)
+        crop_img = image.crop(
+            (int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+            )
+        return crop_img
+    
+
+class DamageDetector():
+    def __init__(self, config, checkpoint, threshold=0.5, model_device='cpu'):
+        self.model = None
+        self.classes = None
+        self.class_metadata = None
+        self.config = config
+
+        self.checkpoint = checkpoint
+        self.threshold = threshold
+        self.model_device = model_device
+        self.initialise()
+
+    def initialise(self):
+        self.classes = {
+            'scratch': 0,
+            'paint_deterioration': 1,
+            'dirt': 2,
+            'dent': 3,
+            'reflection': 4,
+        }
+        self.class_metadata = MetadataCatalog.get('_').set(
+            thing_classes=list(self.classes.keys())
+            )
+        
+        cfg = get_cfg()
+        add_pointrend_config(cfg)
+        cfg.merge_from_file(self.config)
+        cfg.MODEL.WEIGHTS = self.checkpoint
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold
+        cfg.MODEL.DEVICE = self.model_device
+        self.model = DefaultPredictor(cfg)
+    
+    def predict(self, img, filter_class=True):
+        start_time = time.time()
+        self.model.model.eval()
+        predictions = self.model(img)
+        
+        instances = predictions["instances"].to("cpu")
+        
+        if filter_class:
+            # Filter and remove dirt and reflection classses
+            instances = instances[instances.pred_classes != self.classes["dirt"]]
+            instances = instances[instances.pred_classes != self.classes["reflection"]]
+
+        '''
+        # Filter center points to make sure that it is in the segmentation mask
+        for row in instances.pred_boxes.get_centers().numpy().astype('int'):
+            print(mask[row[1], row[0]])
+        '''
+        # TODO: Figure out how to filter using numpy arrays
+
+        v = Visualizer(
+            img_rgb=img,
+            metadata=self.class_metadata,
+            instance_mode=ColorMode.SEGMENTATION
+            )
+
+        out = v.draw_instance_predictions(
+            instances
+            )
+            
+        result = {
+            'image' : out,
+            'scratch_count' : len(instances[instances.pred_classes == self.classes["scratch"]]),
+            'paint_deterioration_count' : len(instances[instances.pred_classes == self.classes["paint_deterioration"]]),
+            'dent_count' : len(instances[instances.pred_classes == self.classes["dent"]]),
+        }
+
+        return result, instances
+
--- localise_damage.py
+++ localise_damage.py
@@ -1,61 +1,93 @@
+from curses import panel
 from test import PanelDetector, CarDetector, DamageDetector
 from shapely import geometry
 from detectron2.utils.visualizer import GenericMask
-from dictionary import panel_classes, damage_d
+from yaml import parse
+from dictionary import panel_classes, damage_d, old_damage_d
 from PIL import Image
 import numpy as np
+import os
+import json
+import warnings
+warnings.filterwarnings('ignore')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', help='path to image folder')
+args = parser.parse_args()
 
 def create_panel_dic(panel_instances, panel_classes):
     panel_dic = {}
-
+    panel_area = {}
     for i in range(len(panel_instances)):
         inst_mask = panel_instances[i].pred_masks.numpy()
         pred_class = panel_instances[i].pred_classes
         panel_name = [k for k, v in panel_classes.items() if v == pred_class]
         r, w, h = inst_mask.shape    
         panel_mask = GenericMask(inst_mask.reshape(w,h), w, h)
-        
+        polygon_area = []
+        # print(len(panel_mask.polygons))
         for x in range(len(panel_mask.polygons)):
             polygon = panel_mask.polygons[x].reshape(-1,2)
+            # print(polygon)
             if panel_name[0] not in panel_dic:
                 panel_dic[panel_name[0]] = list()
+                panel_area[panel_name[0]] = list()
             panel_dic[panel_name[0]].append(geometry.Polygon(polygon))
+            polygon_area.append(geometry.Polygon(polygon).area)
+        # print('max panel polygon area: ',np.max(polygon_area))
 
-    return panel_dic
+        panel_area[panel_name[0]].append(np.mean(polygon_area))
+
+    return panel_dic, panel_area
 
 def create_damage_dic(damage_instances, damage_d):
     damage_dic = {}
+    area_dic = {}
     # list of damage centroids
     damage_centroids = damage_instances.pred_boxes.get_centers().cpu().numpy()
-
     damage_classes = damage_instances.pred_classes.to('cpu').numpy()
     for i, id in enumerate(damage_classes):
         class_name = [v for k,v in damage_d.items() if k==id]
-        if class_name[0] not in damage_dic:
+
+        dam_mask = damage_instances[i].pred_masks.to('cpu').numpy()
+        r, w, h = dam_mask.shape
+        d_mask = GenericMask(dam_mask.reshape(w,h), w, h)
+        polygons_area = []
+        for x in range(len(d_mask.polygons)):
+            d_polygon = d_mask.polygons[x].reshape(-1,2)
+            # print(d_polygon)
+            polygons_area.append(geometry.Polygon(d_polygon).area)
+
+        # print('damage polygn area: ', polygons_area)
+        
+        if class_name[0] not in area_dic:
+            area_dic[class_name[0]] = list()
             damage_dic[class_name[0]] = list()
+        area_dic[class_name[0]].append(np.mean(list(polygons_area)))
         damage_dic[class_name[0]].append(geometry.Point(damage_centroids[i]))
+    
+    return damage_dic, area_dic
 
-    return damage_dic
-
-def locate_point(damage_dic, panel_dic):
+def locate_point(damage_dic, panel_dic, damage_area_dic, panel_area):
     summary_d = {}
     for (damage_k,damage_v) in damage_dic.items():
-        for damage in damage_v:
+        for num,damage in enumerate(damage_v):
+            # print(num)
             found = False
-            #print(damage)
             if not found:
                 for (panel_key, panel_val) in panel_dic.items():
                     if panel_key not in summary_d:
                         summary_d[panel_key] = dict()
+
                     for polygon in panel_val:
                         if polygon.contains(damage):
                             if damage_k not in summary_d[panel_key]:
-                                summary_d[panel_key][damage_k] = 1
-                            else:
-                               summary_d[panel_key][damage_k] += 1 
+                                summary_d[panel_key][damage_k] = list()
+                            # print(panel_area[panel_key])
+                            damage_area_percent = (damage_area_dic[damage_k][num]/panel_area[panel_key])*100
+                            summary_d[panel_key][damage_k].append(damage_area_percent.tolist()) 
                             print('{} detected on {}!'.format(damage_k, panel_key))
-                            found = True
-                            break
                 
                     if found:
                         break
@@ -63,41 +95,88 @@ def locate_point(damage_dic, panel_dic):
                 print('not located')
     return summary_d
 
+def create_img_summary(img_path, panel_detector, damage_detector, car_detector, dam_d, pan_d):
+
+    input_img = np.array(Image.open(img_path).convert('RGB'))
+    # get cropped img 
+    cropped, cropped_mask = car_detector.predict(input_img)
+
+    # detect panels
+    car_results, panel_instances = panel_detector.predict(np.array(cropped))
+
+    # detect damage
+    results, damage_instances = damage_detector.predict(np.array(cropped), filter_class=True)
+    
+    damage_dic, damage_area_dic = create_damage_dic(damage_instances, dam_d)
+
+    panel_dic, panel_area = create_panel_dic(panel_instances, pan_d)
+
+    Image.fromarray(results['image'].get_image()).save('{}.jpg'.format(img_path.split('/')[-1].split('.')[0]))
+
+    Image.fromarray(car_results['image'].get_image()).save('{}_cropped.jpg'.format(img_path.split('/')[-1].split('.')[0]))
+
+    damage_panel_d = locate_point(damage_dic, panel_dic, damage_area_dic, panel_area)
+
+    return damage_panel_d
+
+def car_damage_area(sum_d):
+    car_summary = {}
+    for img ,summary in sum_d.items():
+        im_panel_damage = summary
+        # print(im_panel_damage)
+        for panel, damage_panel in im_panel_damage.items():
+            # print(panel, damage_panel)
+            for damage_name, area in damage_panel.items():
+                print(damage_name, np.sum(area), 'in', panel)
+                if panel not in car_summary:
+                    car_summary[panel] = np.sum(area)
+                else:
+                    car_summary[panel] += np.sum(area)
+    return car_summary
+            
+ 
+
 if __name__ == "__main__":
 
-    # Detector Objects
     panel_detector = PanelDetector(
-        panel_config='carro-ds-cv-carro_locus/license_plate_masking/configs/pointrend_rcnn_X_101_32x8d_FPN_3x_coco.yaml',
-        panel_checkpoint='model.pth',
-        )
+    panel_config='carro-ds-cv-carro_locus/license_plate_masking/configs/pointrend_rcnn_X_101_32x8d_FPN_3x_coco.yaml',
+    panel_checkpoint='model.pth',
+    )
 
-    # Create CarDetector object
-    car_detector = CarDetector(     
+    car_detector = CarDetector(
     config='damage_detection_data_batch_1/pointrend_rcnn_R_50_FPN_3x_coco.yaml',
     checkpoint='car_detection_model/car_detector_1.pth'
     )
+    
 
+    # Create DamageDetector object
     damage_detector = DamageDetector(
-        config='damage_detection_data_batch_1/pointrend_rcnn_R_50_FPN_3x_coco.yaml',
-        checkpoint='damage_model/damage_detector_1.pth',
-        )
+    config='damage_detection_data_batch_1/pointrend_rcnn_R_50_FPN_3x_coco.yaml',
+    checkpoint='damage_model/damage_detector_1.pth',
+    threshold=0.8
+    )
 
-        # Detect Image
-    im_path = '/Users/maixueqiao/Downloads/Project/panel_detection/images/carro_4bUmlSxCVvsYBSoX.jpg'
-    img = np.asarray(Image.open(im_path).convert('RGB'))
+    # read images in folder
+    # folder = '/Users/maixueqiao/Downloads/Project/panel_detection/359777'
+    folder = args.input
 
-    # Panel Detection inference
-    car_results, panel_instances = panel_detector.predict(img)
-    out_im = Image.fromarray(car_results['image'].get_image())
+    summary_d = dict()
 
-    # # Car detection inference
-    cropped, cropped_mask = car_detector.predict(img)
-    # cropped.save('crop.png')
+    for x, filename in enumerate(os.listdir(folder)):
+        img_path = os.path.join(folder,filename)
+        img_summary = create_img_summary(img_path, panel_detector, damage_detector, car_detector, old_damage_d, panel_classes)
+        if img_summary:
+            summary_d[filename] = img_summary
+        else:
+            summary_d[filename] = {}
 
-    # Damage Detection
-    results, damage_instances = damage_detector.predict(np.array(img), filter_class=True)
-    Image.fromarray(results['image'].get_image()).save('out8.jpg')
+    print(summary_d)
+    with open("output.json", "w") as f:
+        json.dump(summary_d, f, indent=4)
+    
+    # creeate per car damage summary
+    car_damage_sum = car_damage_area(summary_d)
+    with open("car_damage_area.json", "w") as output_f:
+        json.dump(car_damage_sum, output_f, indent=4)
 
-    damage_dic, panel_dic = create_damage_dic(damage_instances, damage_d), create_panel_dic(panel_instances, panel_classes)
-    damage_panel_d = locate_point(damage_dic, panel_dic)
-    print(damage_panel_d)
+ 
